b8f57f0db3279339efa520f0f80f4a8a1aec166a
Piccolo Luca <l.piccolo@reply.it>
2017-12-16 17:49:36 +0100

    first commit

---
+++ Chapter 2.R
@@ -0,0 +1,13 @@
+#Exercises from book "An introduction to statistical learning: with applications in R"
+#Chapter 2.3 - Lab: Introduction to R
+
+#create vectors
+x <- c(1,3,2,5)
+x
+x = c(1,6,2)
+y = c(1,4,3)
+y
+#sum vectors
+length(x)
+length(y)
+x+y
