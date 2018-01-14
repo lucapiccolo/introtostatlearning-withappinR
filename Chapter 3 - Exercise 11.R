c813a504246d4b90a55f5ef27e977ed3175be853
Piccolo Luca <l.piccolo@reply.it>
2018-01-14 22:04:47 +0100

    started exc 11

---
+++ Chapter 3 - Exercise 11.R
@@ -0,0 +1,23 @@
+#Exercises from book "An introduction to statistical learning: with applications in R"
+#Chapter 3.7 - Exercise 11
+
+set.seed(1)
+x = rnorm(100)
+y = 2*x+rnorm(100)
+plot(x,y)
+
+#Perform linear regression without intercept of y onto x
+lm.fit <- lm(y~x+0)
+abline(lm.fit)
+summary(lm.fit) #est: 1.9939, stderr: 0.1065, t: 18.73, pval: <2e-16
+#Strong evidence of relation (about 2 increase in y for each increase in x)
+
+#Perform linear regression without intercept of x onto y
+lm.fit.inv <- lm(x~y+0)
+plot(y,x)
+abline(lm.fit.inv)
+summary(lm.fit.inv) #est: 0.39111, stderr: 0.02089, t: 18.73, pval: <2e-16
+#Strong evidence of relation (about 0,4 increase in x for each increase in y)
+
+#What is the relationship?
+#The normal component (y = 2*x+norm) is such that est1 != 1/est2
